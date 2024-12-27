6561c8771a4f716cf5ea290a4bb12d2364b73acf
HazemHamdyA <hazemhamdya>
2024-12-27 17:56:39 +0200

    Add multi-step forms for registration and password recovery
    Implemented multi-step forms for user registration and password recovery processes. These include components for each step and ensure a smoother UI flow for account creation and password resets.

---
+++ components/ForgetPassword/StepThree.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const emits = defineEmits(['change:step-three'])
+
+const isInputOneShow =ref(false)
+const isInputTwoShow =ref(false)
+</script>
+
+<template>
+  <div>
+    <h3>كلمة السر الجديدة </h3>
+    <span>قم بادخال كلمة المرور الجديدة مرتين ثم ستتمكن من فتح الحساب</span>
+  </div>
+  <v-row>
+    <v-col cols="6" sm="12" class="pt-0">
+      <v-text-field
+          label=""
+          :type="isInputOneShow ? 'text' : 'password'"
+          rounded="lg"
+          variant="outlined"
+          density="compact"
+          placeholder="كلمة المرور الجديدة"
+          hide-details
+      >
+        <template #append-inner>
+          <div class="cursor-pointer" @click="isInputOneShow = !isInputOneShow">
+            <v-icon v-if="!isInputOneShow">mdi-eye-off</v-icon>
+            <v-icon v-else>mdi-eye</v-icon>
+          </div>
+        </template>
+      </v-text-field>
+    </v-col>
+    <v-col cols="6" sm="12" class="pt-0">
+      <v-text-field
+          label=""
+          :type="isInputTwoShow ? 'text' : 'password'"
+          rounded="lg"
+          variant="outlined"
+          density="compact"
+          placeholder="كلمة المرور مجددا"
+          hide-details
+      >
+        <template #append-inner>
+          <div class="cursor-pointer" @click="isInputTwoShow = !isInputTwoShow">
+            <v-icon v-if="!isInputTwoShow">mdi-eye-off</v-icon>
+            <v-icon v-else>mdi-eye</v-icon>
+          </div>
+        </template>
+      </v-text-field>
+    </v-col>
+    <v-col cols="12" sm="12" class="pt-0">
+      <v-btn @click="emits('change:step-three')" color="primary" round block size="50" rounded="lg">حفظ</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ pages/forget-password.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useDisplay } from 'vuetify'
+import StepOne from "~/components/ForgetPassword/StepOne.vue";
+import StepTwo from "~/components/ForgetPassword/StepTwo"
+import StepThree from "~/components/ForgetPassword/StepThree.vue";
+
+const { mobile } = useDisplay()
+const currentStep =ref(0)
+const forgetPasswordSteps = ref([
+  StepOne,
+  StepTwo,
+  StepThree
+])
+</script>
+
+<template>
+  <v-container fluid >
+    <v-row class="justify-space-around ga-md-16 align-center">
+      <v-col cols="12" sm="12" md="4">
+        <SvgIcon name="samnan-panner" class=" rounded-xl cursor-auto" :style="`max-height: 90vh; ${mobile ? 'width: 100%' : ''}`"/>
+      </v-col>
+      <v-col cols="12" sm="6" md="4" class="d-flex flex-column ga-4">
+        <div class="d-flex ga-2 align-center">
+          <SvgIcon name="logo"/>
+          <SvgIcon name="title"/>
+        </div>
+        <component :is="forgetPasswordSteps[currentStep]"
+                   @change:step-one="currentStep = 1"
+                   @change:step-two="currentStep = 2"
+                   @change:step-three="currentStep = 0"
+        ></component>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
