f6f1e36471c6be0d16d0a3539485e323c32cded8
hazemhamdyabdo <hazemhamdy117@gmail.com>
2025-02-22 19:31:27 +0200

    feat(Dashboard): update layout for charts and remove unused success SVG assets

--- pages/technical/dashboard.vue
+++ pages/technical/dashboard.vue
@@ -62,10 +62,10 @@ const loadMore = async () => {
 </script>
 <template>
   <v-row>
-    <v-col cols="6">
+    <v-col cols="12" lg="6">
       <DoneRequestsChart />
     </v-col>
-    <v-col cols="6">
+    <v-col cols="12" lg="6">
       <AllRequestsChart />
     </v-col>
     <v-col cols="12">
